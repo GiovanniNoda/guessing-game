58cf9f736a2efc6fd859826dce9f25be6dd2610c
Giovanni <giovanniminoru@gmail.com>
2025-02-10 21:43:55 -0300

    theme, font, global styles

---
+++ jogo-advinhacao/src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+    white: '#FFFFFF',
+    'gray-100': '#F5F5FA',
+    'gray-200': '#C3C3C3',
+    'gray-300': '#AFAFAF',
+    'gray-400': '#9B9B9B',
+
+    black: '#000000',
+
+    'blue-100': '#EBEBFC',
+    'blue-200': '#6363DB',
+    'blue-300': '#4F4FB9',
+    'blue-400': '#4F4FB9',
+
+    'orange-100': '#FFCF62',
+    'orange-200': '#E8891C',
+    'orange-300': '#691B0A',
+
+    'green-100': '#E1F5EC',
+    'green-200': '#03AB4F',
+}
